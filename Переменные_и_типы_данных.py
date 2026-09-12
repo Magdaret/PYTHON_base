340f7d67a514adf9fded794e92f5bea0ef5065ac
YaDolgoSry <gdg9511@gmail.com>
2026-09-12 19:01:07 +0300

    Base is base after all

---
+++ Переменные_и_типы_данных.py
@@ -0,0 +1,37 @@
+# Переменные
+product = 'bananas'
+price = '100$'
+enter = ' '
+print(product + enter + price)
+
+# Boolean выражения
+booleans1 = True
+booleans2 = False
+print(booleans1, booleans2)
+
+#Тип int
+lesson_1 = 8
+lesson_2 = 9
+lesson_3 = 13
+
+#Str
+text = ('''Laudate omnes gentes laudate
+Magnificat in secula
+Et anima mea laudate
+Magnificat in secula 
+''')
+print(text)
+
+#Вставка значений в строку
+userName = "Tom"
+userAge = 37
+user = f"name: {userName}  age: {userAge}"
+print(user)   # name: Tom  age: 37
+
+#Определение типа встроенной функции
+userId = "abc"      # тип str
+print(type(userId)) # <class 'str'>
+ 
+userId = 234        # тип int
+print(type(userId)) # <class 'int'>
+
